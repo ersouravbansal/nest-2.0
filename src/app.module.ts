c4d1a8c552dbe317c43b06a87c8298e3ebe63022
Sourav <sourav@ndtvs-MacBook-Air.local>
2024-09-20 13:00:06 +0530

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) { 
+    //1st option
+    // consumer.apply(LoggerMiddleware).forRoutes('songs');
+
+    //2nd option
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'songs', method: RequestMethod.POST });
+    //3rd option
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+  }
+}
